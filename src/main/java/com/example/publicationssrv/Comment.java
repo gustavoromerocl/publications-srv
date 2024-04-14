3d39c3a8d19e2ac173c6437b523a3d2e199066a3
romerodevelopments <gandresrp@gmail.com>
2024-04-14 19:26:38 -0400

    feat: add comment entity class

---
+++ src/main/java/com/example/publicationssrv/Comment.java
@@ -0,0 +1,36 @@
+package com.example.publicationssrv;
+
+import com.example.publicationssrv.model.Publication;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "publication_comment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "comment_text")
+    private String commentText;
+
+    @ManyToOne
+    @JoinColumn(name = "publication_id")
+    private Publication publication;
+}
