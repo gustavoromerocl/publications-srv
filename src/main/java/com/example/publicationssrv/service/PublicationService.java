fdde0b66f8b7307550531c1a886edf94066c6010
romerodevelopments <gandresrp@gmail.com>
2024-04-14 19:29:12 -0400

    feat: add publication service

---
+++ src/main/java/com/example/publicationssrv/service/PublicationService.java
@@ -0,0 +1,14 @@
+package com.example.publicationssrv.service;
+
+import com.example.publicationssrv.model.Publication;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PublicationService {
+    List<Publication> getAllPublications();
+    Optional<Publication> getPublicationById(Long id);
+    Publication createPublication(Publication publication);
+    // Publication updatePublication(Long id, Publication publication);
+    // Publication deletePublication(Long id);
+}
---
+++ src/main/java/com/example/publicationssrv/service/PublicationServiceImp.java
@@ -0,0 +1,31 @@
+package com.example.publicationssrv.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.publicationssrv.model.Publication;
+import com.example.publicationssrv.repository.PublicationRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PublicationServiceImp implements PublicationService{
+    @Autowired
+    private PublicationRepository publicationRepository;
+
+    @Override
+    public List<Publication> getAllPublications() {
+        return publicationRepository.findAll();
+    }
+
+    @Override
+    public Optional<Publication> getPublicationById(Long id) {
+        return publicationRepository.findById(id);
+    }
+
+    @Override
+    public Publication createPublication(Publication publication) {
+        return publicationRepository.save(publication);
+    }
+}
