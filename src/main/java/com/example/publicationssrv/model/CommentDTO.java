1ef450da39019a0153a9459d3afcb86394a1409b
romerodevelopments <gandresrp@gmail.com>
2024-04-14 19:25:45 -0400

    feat: add comment DTO

---
+++ src/main/java/com/example/publicationssrv/model/CommentDTO.java
@@ -0,0 +1,12 @@
+package com.example.publicationssrv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CommentDTO {
+    private String commentText;
+}
