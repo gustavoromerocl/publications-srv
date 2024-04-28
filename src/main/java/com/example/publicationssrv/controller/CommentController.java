abd9c5bc6ed57f984c5260f8e326eed1e7e2f0f9
romerodevelopments <gandresrp@gmail.com>
2024-04-28 19:43:09 -0400

    chore: remove unused code

--- src/main/java/com/example/publicationssrv/controller/CommentController.java
+++ src/main/java/com/example/publicationssrv/controller/CommentController.java
@@ -1,7 +1,5 @@
 package com.example.publicationssrv.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
