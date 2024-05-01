04ca3e1f486ff78ace0b7e85ca8319fd08d4cac3
romerodevelopments <gandresrp@gmail.com>
2024-05-01 15:52:10 -0400

    fix: resolve execution test bug

--- src/test/java/com/example/controller/PublicationControllerTest.java
+++ src/test/java/com/example/controller/PublicationControllerTest.java
@@ -1,43 +1,45 @@
-// package com.example.controller;
+package com.example.controller;
 
-// import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
 
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.test.context.ContextConfiguration;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-// import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-// import com.example.publicationssrv.controller.PublicationController;
-// import com.example.publicationssrv.model.Publication;
-// import com.example.publicationssrv.service.PublicationServiceImp;
+import com.example.publicationssrv.PublicationssrvApplication;
+import com.example.publicationssrv.controller.PublicationController;
+import com.example.publicationssrv.model.Publication;
+import com.example.publicationssrv.service.PublicationServiceImp;
 
 
-// @WebMvcTest(PublicationController.class)
-// public class PublicationControllerTest {
-//   @Autowired
-//   private MockMvc mockMvc;
+@WebMvcTest(PublicationController.class)
+@ContextConfiguration(classes=PublicationssrvApplication.class)
+public class PublicationControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
 
-//   @MockBean
-//   private PublicationServiceImp publicationServiceMock;
+  @MockBean
+  private PublicationServiceImp publicationServiceMock;
 
-//   @Test
-//   public void getAllTest() throws Exception {
-//     Publication publication1 = new Publication();
-//     publication1.setTitle("Título 1");
-//     Publication publication2 = new Publication();
-//     publication1.setTitle("Título 2");
+  @Test
+  public void getAllTest() throws Exception {
+    Publication publication1 = new Publication();
+    publication1.setTitle("Título 1");
+    Publication publication2 = new Publication();
+    publication2.setTitle("Título 2");
 
-//     List<Publication> publications = Arrays.asList(publication1, publication2);
-//     when(publicationServiceMock.getAllPublications()).thenReturn(publications);
+    List<Publication> publications = Arrays.asList(publication1, publication2);
+    when(publicationServiceMock.getAllPublications()).thenReturn(publications);
 
-//     mockMvc.perform(MockMvcRequestBuilders.get("/students"))
-//       .andExpect(MockMvcResultMatchers.status().isOk());
-//   }
-// }
+    mockMvc.perform(MockMvcRequestBuilders.get("/publications"))
+      .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+}
--- src/test/java/com/example/repository/PublicationRepositoryTest.java
+++ src/test/java/com/example/repository/PublicationRepositoryTest.java
@@ -1,32 +1,34 @@
-// package com.example.repository;
+package com.example.repository;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-// import com.example.publicationssrv.model.Publication;
-// import com.example.publicationssrv.repository.PublicationRepository;
+import com.example.publicationssrv.PublicationssrvApplication;
+import com.example.publicationssrv.model.Publication;
+import com.example.publicationssrv.repository.PublicationRepository;
 
-// @DataJpaTest
-// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// @SpringBootTest(classes = PublicationRepository.class)
-// public class PublicationRepositoryTest {
-//   @Autowired
-//   private PublicationRepository publicationRepository;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes=PublicationssrvApplication.class)
+public class PublicationRepositoryTest {
+  @Autowired
+  private PublicationRepository publicationRepository;
 
-//   @Test
-//   public void savePublicationTest() {
-//     Publication publication = new Publication();
-//     publication.setTitle("Receta de lasagna");
+  @Test
+  public void savePublicationTest() {
+    Publication publication = new Publication();
+    publication.setTitle("Receta de lasagna");
 
-//     Publication result = publicationRepository.save(publication);
+    Publication result = publicationRepository.save(publication);
 
-//     assertNotNull(result.getId());
-//     assertEquals("Receta de lasagna", result.getTitle());
-//   }
-// }
+    assertNotNull(result.getId());
+    assertEquals("Receta de lasagna", result.getTitle());
+  }
+}
